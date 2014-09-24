df4e7bd3248a0acdaa10b6b2d1c9ac582b743330
drewmcarthur <drewmcarthur1@gmail.com>
2014-09-24 19:02:18 -0400

    working version

--- app.js
+++ app.js
@@ -29,10 +29,12 @@ app.configure('production', function(){
 
 // Routes
 
-//app.get('/', routes.index);
+app.get('/', routes.index);
+/*
 app.get('/', function(req, res){
   res.render('index.html');
 });
+*/
 
 app.listen(3000, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
