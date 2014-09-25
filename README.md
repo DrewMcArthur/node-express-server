42ef18895736f12acd7a25fb3b9e525228355733
drewmcarthur <drewmcarthur1@gmail.com>
2014-09-24 20:45:40 -0400

    removed more complexities, works completely.

--- README.md
+++
@@ -1,4 +0,0 @@
-node-express-server
-===================
-
-An attempt at a web server using Node.js and Express
--- app.js
+++ app.js
@@ -2,7 +2,9 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res){
-  res.send('hello world');
+  res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(3000);
+app.listen(1337, function(){
+	console.log("Server is running on port 1337");
+});
