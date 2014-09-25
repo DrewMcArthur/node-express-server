978a3403d10bdf68ed9ac9f29b7ded68a300ca51
drewmcarthur <drewmcarthur1@gmail.com>
2014-09-25 16:38:38 -0400

    added other stuff to server

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ app.get('/', function(req, res){		//when someone loads '/' (the home page)
 	console.log('Someone loaded \'/\'!');
 });
 
+app.get(/^\/name\/(\w+)(?:\.\.(\w+))?$/, function(req, res){
+	var name = req.params[0];
+	res.send('Hello ' + name);
+});
+
 app.listen(1337, function(){ //listen for requests at ipaddress:1337
 	console.log("Server is running on port 1337");		//callback function, completely optional.
 });
