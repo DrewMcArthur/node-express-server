ee87098cfa272f25ecfdcab04e32b56e21f2aa7f
drewmcarthur <drewmcarthur1@gmail.com>
2014-09-24 20:56:34 -0400

    added comments to clarify how stuff works

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+app.get('/', function(req, res){		//when someone loads '/' (the home page) 
+  res.sendFile(__dirname + '/index.html');		//then send them the file located at '/index.html'.  
+	console.log('Someone loaded \'/\'!');
 });
 
-app.listen(1337, function(){
-	console.log("Server is running on port 1337");
+app.listen(1337, function(){ //listen for requests at ipaddress:1337
+	console.log("Server is running on port 1337");		//callback function, completely optional.
 });
