2a04caa659a1135cb7d6a57e6513f1cce6d681ef
ubuntu <drewmcarthur1@gmail.com>
2014-09-26 17:51:53 -0400

    simplified server file

--- app.js
+++ app.js
@@ -1,18 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + '/public'));
-
-app.get('/', function(req, res){		//when someone loads '/' (the home page) 
-	res.sendFile(__dirname + '/index.html');		//then send them the file located at '/index.html'.  
-	console.log('Someone loaded \'/\'!');
-});
-
-app.get(/^\/name\/(\w+)(?:\.\.(\w+))?$/, function(req, res){
-	var name = req.params[0];
-	console.log(name);
-	res.send('Hello ' + name);
-});
+app.use(express.static(__dirname + '/public'));//this hosts the files located in the ./public directory
 
 app.listen(1337, function(){ //listen for requests at ipaddress:1337
 	console.log("Server is running on port 1337");		//callback function, completely optional.
