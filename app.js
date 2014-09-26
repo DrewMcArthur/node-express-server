dc51874faed7064db1dbd983d60d8f92a14ed9db
ubuntu <drewmcarthur1@gmail.com>
2014-09-26 16:05:36 -0400

    added lines to server.js

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 var express = require('express');
 var app = express();
+var http = require('http').Server(app);
 
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res){		//when someone loads '/' (the home page) 
 	res.sendFile(__dirname + '/index.html');		//then send them the file located at '/index.html'.  
 	console.log('Someone loaded \'/\'!');
+});
+
+app.get('/chat', function(req,res){
+	res.sendFile(__dirname + '/chat.html');
+	console.log('Someone loaded the chat!');
 });
 
 app.get(/^\/name\/(\w+)(?:\.\.(\w+))?$/, function(req, res){
