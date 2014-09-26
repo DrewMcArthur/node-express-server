4e260ffc8cfafbfda44b4a381a2522f472322fbc
ubuntu <drewmcarthur1@gmail.com>
2014-09-26 17:49:55 -0400

    chat server works at /chat.html

--- temp.js
+++ temp.js
@@ -3,19 +3,17 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.use(express.static(__dirname + '/public'));
 
 io.on('connection', function(socket){
-        socket.on('chat message', function(msg){
-                io.emit('chat message', msg);
-                console.log(msg);
-        });
+	console.log('connected');
+	socket.on('msgr',function(socket){
+		io.emit('msgs', msg);
+		console.log(msg);
+	});
 });
+
+app.use(express.static(__dirname + '/public'));
 
 http.listen(1337, function(){ //listen for requests at ipaddress:1337
 	console.log("Server is running on port 1337");		//callback function, completely optional.
 });
-
-
-
-
