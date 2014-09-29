f0f7dcb3db749e6be5a5e5883767bba237ca0abf
ubuntu <drewmcarthur1@gmail.com>
2014-09-29 15:59:02 -0400

    added users online, now to remove them from the list on disconnect

--- app.js
+++ app.js
@@ -2,20 +2,29 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var names = new Array();
+var numOfUsersOnline = 0;
 
 app.use(express.static(__dirname + '/public'));
 
 io.on('connection', function(socket){
-	console.log('connection made');
+	
+	numOfUsersOnline++;
 	socket.on('disconnect', function(){
-		var name = "A user";	//function doesn't have input yet for name
+		name = "A user";	//function doesn't have input yet for name
 		var left = name + " left the chat.";
 		io.emit('chat message', left);
+		numOfUsersOnline--; //still need to remove name from names array
 	});
 	socket.emit('ask name');
 	socket.on('answer name', function(name){
-		var entered = name + " entered the chat!";
+		var entered = name + " entered the chat!"; //a person just entered the chat
+		names[names.length] = name + " "; //all names of people online
+		var nOnline = numOfUsersOnline + " users are online."; // number of people online
+		var uOnline = "Users " + names + " are online."; // users online
 		io.emit('chat message', entered);
+		socket.emit('chat message', nOnline);
+		socket.emit('chat message', uOnline);
 	});
 	socket.on('chat message', function(msg){
 		io.emit('chat message', msg);
