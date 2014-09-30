593177a8add1bf9f350cdeb27d5c28455f312194
drewmcarthur <drewmcarthur1@gmail.com>
2014-09-29 21:34:05 -0400

    fixed uid stuff

--- app.js
+++ app.js
@@ -2,37 +2,65 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var fs = require('fs');
 
 var users = new Array(); // array of users online by UID
 var numOfUsersOnline = 0; // incremented and decremented on connect and disconnect
 var userList = new Array();  // array of users online in order (no holes in index)
-var uidCeil = 100; // UID will be random number between 1 and uidCeil
+var uidCeil = 10000; // UID will be random number between 1 and uidCeil
 
 app.use(express.static(__dirname + '/public')); // allow access to all files in ./public
 
 io.on('connection', function(socket){ //on connection to a socket,
+	
+	var random = Math.ceil(Math.random() * uidCeil); //set a UID to a random number
 
-	var UID = Math.ceil(Math.random() * uidCeil); //set a UID to a random number
-
-	while(users[UID] != null){ // try doing this, and if the UID is taken, keep repeating it
-		UID = Math.ceil(Math.random() * uidCeil); // get new random number for UID
+	while(users[random] != null){ // try doing this, and if the UID is taken, keep repeating it
+		if(users[random] != null){
+			random = Math.ceil(Math.random() * uidCeil); // get new random number for UID
+		}
+/*  attempt at error if while loop goes too long.  just have to hope that doesn't happen, i guess.
+ 
+		if(10000<ifThisGetsTooBigThenQuit){
+			fs.writeFile("/error.log", "The while loop ran for too long, sorry. Here's the random number we stopped on: "+ random, function(err) {
+				if(err) {
+					console.log(err);
+				} else {
+					console.log("The file was saved!");
+				}
+			}); 
+			process.exit("error");
+		}; // if the while loop goes for too long, then stop the server, and save an error.log
+*/
 	}
+	
+	var UID = random;
+
+	console.log("UID");
+	console.log(UID);
 
 	socket.emit('ask name'); //ask the server what the user's name is 
 
 	socket.on('answer name', function(name){ // when the client responds, 
 		var entered = name + " entered the chat!"; //a person just entered the chat
 		users[UID] = name; //all names of people online
-		console.log("user " + UID + " is online with name " + users[UID] + ";"); // notify server that user is online
+		console.log("user " + UID + " with name " + users[UID] + " is online;"); // notify server that user is online
 		numOfUsersOnline++; // increase number online by 1
 		var nOnline = numOfUsersOnline + " users are online."; // number of people online
 		var uOnline = "Online: " + userList; // users online
-		makeUserList(); //iterate through users, and make an array without all of the holes
 
+		makeUserList(); //iterate through users, and make an array without all of the holes
+		console.log(userList); // log users online
 		//says [user entered] to everyone, tells newb how many and who is online.
-		socket.emit('chat message', nOnline); // tell client that just entered how many are online
-		socket.emit('chat message', uOnline); // tell client that just entered who is online
-		io.emit('chat message', entered);//this should be changed so that it only broadcasts to all of the other sockets, and not itself.
+		
+		if(numOfUsersOnline>1){
+			socket.emit('chat message', nOnline); // tell client that just entered how many are online
+			socket.emit('chat message', uOnline); // tell client that just entered who is online
+			io.emit('chat message', entered);//this should be changed so that it only broadcasts to all of the other sockets, and not itself.
+		} else {
+			socket.emit('chat message', "You are the only user currently online.");
+		}
+
 	}); // end what happens when the client answers with the user's name
 
 	socket.on('typing check', function(name){
@@ -49,8 +77,9 @@ io.on('connection', function(socket){ //on connection to a socket,
 		io.emit('chat message', left); 
 		numOfUsersOnline--; //still need to remove name from names array
 		users[UID] = null; //removes userid from array of taken uids
-		if(users[UID] !=null ){console.log("ERRRORRRRRR the uid of the user that just left is not null.  why'd that happen? D:");}	//error message just in case the line before didn't work.
 		makeUserList(); //see :30
+		console.log("user " + UID + " with name " + users[UID] + " is offline;"); // notify server that user is offline
+		console.log(userList); // log users online
 	});
 
 });
