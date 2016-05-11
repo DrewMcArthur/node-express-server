f301b5067e348323c85c39faeead146830b6015a
Ubuntu <ubuntu@ip-172-31-51-38.ec2.internal>
2016-05-11 18:06:39 +0000

    added download counter

--- app.js
+++ app.js
@@ -7,6 +7,20 @@ var liner = new stream.Transform( { objectMode: true } );
 
 app.use(express.static(__dirname + '/public')); // allow access to all files in ./public
 
+app.get('/download', function(req, res){
+	//count number of downloads in file
+	var counter = __dirname + '/public/dlnum.txt';
+	var n = fs.readFileSync(counter, 'utf-8').match(/\d+/)[0];
+	n++;
+	fs.writeFile(counter, n, function(err){
+		if (err) throw err;
+	});
+
+	//download the zip file
+	var dlfile = __dirname + '/public/cul.zip';
+	res.download(dlfile);
+});
+
 http.listen(80, function(){ //listen for requests at ipaddress:80
 	console.log('Server is running on port 80');  //callback function, completely optional.
 });
