843351aa04ad1af4ce33602316add649f356e25d
DrewMcArthur <drewmcarthur1@gmail.com>
2016-05-20 20:26:57 +0000

    added SSL and improved the little things that matter

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
+var https = require('https');
 var fs = require('fs');
-var stream = require('stream');
-var liner = new stream.Transform( { objectMode: true } );
 
-app.use(express.static(__dirname + '/public')); // allow access to all files in ./public
+var options = {
+  key: fs.readFileSync('/etc/letsencrypt/live/drewdiezel.xyz-0001/privkey.pem', 'utf-8'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/drewdiezel.xyz-0001/fullchain.pem', 'utf-8')
+};
+
+
+//ensures all traffic is https
+function requireHTTPS(req, res, next) {
+    if (!req.secure) {
+        return res.redirect('https://' + req.get('host') + req.url);
+    }
+    next();
+}
+
+app.use(requireHTTPS);
+app.use(express.static(__dirname + '/public'));
 
 app.get('/download', function(req, res){
+	console.log(req);
 	//count number of downloads in file
 	var counter = __dirname + '/public/dlnum.txt';
 	var n = fs.readFileSync(counter, 'utf-8').match(/\d+/)[0];
@@ -22,5 +37,9 @@ app.get('/download', function(req, res){
 });
 
 http.listen(80, function(){ //listen for requests at ipaddress:80
-	console.log('Server is running on port 80');  //callback function, completely optional.
+	console.log('HTTP Server listening on port 80.');
+});
+
+https.createServer(options, app).listen(443, function(){
+	console.log('HTTPS Server listening on port 443.');
 });
--- public/script.js
+++ public/script.js
@@ -1,12 +1,17 @@
 $(document).ready(function(){
+	//disable links that aren't working
+	$('.half:nth-child(1) a').addClass('disabled');
+	$('.dl-link').addClass('disabled');
+	$('.disabled').on('click', function(e){
+		e.preventDefault();
+		console.log("That link isn't quite ready yet.");
+	});
+
+	//blurred background when screen is wide enough
 	var w = ($(document).width() - 889)/2;
 	$('.side div').css('width', w + 'px');
-	$('.spotify').on('click', function(e){
-		e.preventDefault();
-		console.log("i don't have a spotify page yet");
+	$(window).on('resize', function(){
+		var w = ($(document).width() - 889)/2;
+		$('.side div').css('width', w + 'px');
 	});
 });
-$(window).on('resize', function(){
-	var w = ($(document).width() - 889)/2;
-	$('.side div').css('width', w + 'px');
-})
