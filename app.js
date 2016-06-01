9206cdf6bec7c47a0413548571c712f51a135b43
DrewMcArthur <drewmcarthur1@gmail.com>
2016-06-01 18:32:12 +0000

    site is online

--- app.js
+++ app.js
@@ -21,21 +21,6 @@ function requireHTTPS(req, res, next) {
 app.use(requireHTTPS);
 app.use(express.static(__dirname + '/public'));
 
-app.get('/download', function(req, res){
-	console.log(req);
-	//count number of downloads in file
-	var counter = __dirname + '/public/dlnum.txt';
-	var n = fs.readFileSync(counter, 'utf-8').match(/\d+/)[0];
-	n++;
-	fs.writeFile(counter, n, function(err){
-		if (err) throw err;
-	});
-
-	//download the zip file
-	var dlfile = __dirname + '/public/cul.zip';
-	res.download(dlfile);
-});
-
 http.listen(80, function(){ //listen for requests at ipaddress:80
 	console.log('HTTP Server listening on port 80.');
 });
--- public/script.js
+++ public/script.js
@@ -1,11 +1,4 @@
 $(document).ready(function(){
-	//disable links that aren't working
-	//$('.half:nth-child(1) a').addClass('disabled');
-	$('.disabled').on('click', function(e){
-		e.preventDefault();
-		console.log("That link isn't quite ready yet.");
-	});
-
 	//blurred background when screen is wide enough
 	var w = ($(document).width() - 889)/2;
 	$('.side div').css('width', w + 'px');
